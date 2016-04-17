399eedab6a252571cdd0d1a6990570520d2ca938
yelonglt <354734713@qq.com>
2016-04-17 17:27:49 +0800

    创建一个基本的Fragment

---
+++ app/src/main/java/com/yelong/androidframeproject/activity/fragment/BaseFragment.java
@@ -0,0 +1,15 @@
+package com.yelong.androidframeproject.activity.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+/**
+ * Created by eyetech on 16/4/17.
+ */
+public class BaseFragment extends Fragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
